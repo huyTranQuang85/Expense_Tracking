fbb176200cc1dca4c2f35ace6c07f71f20e57826
Trần Quang Huy <huy.tranquang.com@gmail.com>
2026-05-15 19:45:41 +0700

    feat: add category controller

---
+++ Backend/src/controllers/categoryController.js
@@ -0,0 +1,70 @@
+const categoryService = require("../services/categoryService");
+
+exports.getMyCategories = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { type } = req.query; // optional ?type=income/expense
+
+    const categories = await categoryService.getCategories(userId, type);
+
+    res.json({
+      status: "success",
+      data: categories,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.createCategory = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const payload = req.body;
+
+    const category = await categoryService.createCategory(userId, payload);
+
+    res.status(201).json({
+      status: "success",
+      data: category,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateCategory = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const categoryId = req.params.id;
+    const payload = req.body;
+
+    const category = await categoryService.updateCategory(
+      userId,
+      categoryId,
+      payload
+    );
+
+    res.json({
+      status: "success",
+      data: category,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const categoryId = req.params.id;
+
+    await categoryService.deleteCategory(userId, categoryId);
+
+    res.json({
+      status: "success",
+      message: "Xóa danh mục thành công",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
